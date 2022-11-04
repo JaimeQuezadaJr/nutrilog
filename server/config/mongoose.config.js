07723c6e6ffdd7be5029add548661b3df4ef3fbc
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-03 19:18:48 -0700

    completed config file database connection

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoURI = 'mongodb://localhost/dietlog';
+
+mongoose.connect(mongoURI)
+    .then(()=> console.log('Established a connection to the database'))
+    .catch(err => console.log('Something went wrong when connecting to the database', err));
