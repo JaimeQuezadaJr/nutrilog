7f13d8dccfdee9da00af5368e9713a254832d5b5
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-04 19:25:50 -0700

    completed jwt config file

--- server/config/jwt.config.js
+++ server/config/jwt.config.js
@@ -1,3 +1,14 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const SECRETKEY = process.env.JWT_SECRET;
+
+module.exports.SECRETKEY = SECRETKEY;
+module.exports.authenticate = (req, res, next) => {
+  jwt.verify(req.cookies.userToken, SECRETKEY, (err, payload) => {
+    if (err) {
+      res.status(401).json({verified: false});
+    } else {
+      next();
+    }
+  });
+}
