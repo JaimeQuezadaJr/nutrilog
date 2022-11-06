fe7111a8fb07ecf9460c0023cfbdd738afdd9949
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-05 18:04:47 -0700

    added port number

--- server/server.js
+++ server/server.js
@@ -4,4 +4,4 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
+const port = process.env.PORT || 4000;
