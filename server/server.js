68e73c1d28239340288c0fb42f8b142568519c8c
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-05 18:06:19 -0700

    added express to server file

--- server/server.js
+++ server/server.js
@@ -5,3 +5,11 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 4000;
+
+// create express app object
+const app = express();
+
+//express
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
