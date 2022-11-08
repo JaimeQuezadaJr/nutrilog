5cc730e7a6b4daad0c74a919570f6c2b95b5ec5e
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-07 17:57:59 -0800

    completed user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -31,3 +31,28 @@ const UserSchema = mongoose.Schema(
     },
     { timestamps: true }, 
 );
+
+UserSchema.virtual('confirmPassword')
+    .get(() => this._confirmPassword)
+    .set((value) => (this._confirmPassword = value));
+
+UserSchema.pre('validate', function (next) {
+    if (this.password !== this.confirmPassword) {
+        this.invalidate('confirmPassword', 'Passwords must match!');
+    }
+    next();
+});
+
+UserSchema.pre('save', async function (next) {
+    console.log('IN PRE SAVE:', this.password);
+    try {
+        const hashedPassword = await bcrypt.hash(this.password, 10);
+        console.log('HASHED:', hashedPassword);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+    console.log('ERROR IN SAVE', error);
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
