a741d8a3eb760f413ea579f157a9b826da534e94
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-07 17:57:21 -0800

    added user schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,2 +1,33 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+
+const UserSchema = mongoose.Schema(
+    {
+        firstName: {
+        type: String,
+        required: [true, 'First Name is required'],
+    },
+        lastName: {
+        type: String,
+        required: [true, 'Last Name is required'],
+    },
+        age: {
+        type: Number,
+        required: [true, 'Age is required'],
+    },
+        email: {
+        type: String,
+        required: [true, 'Email is required'],
+        validate: {
+            validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+            message: "Please enter a valid email"
+          }
+    },
+        password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [8, "Password must be 8 characters or longer"]
+    },
+    },
+    { timestamps: true }, 
+);
