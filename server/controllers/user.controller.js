1de653c78c43a4a638570702e8868fc4e6ef89dd
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-08 18:29:34 -0800

    added register method in user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,3 +3,19 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const SECRET = process.env.JWT_SECRET;
 console.log('SECRET:', SECRET);
+
+const register = async (req, res) => {
+    try {
+        const user = new User(req.body);
+        const newUser = await user.save();
+        console.log('User Created:', newUser);
+        const userToken = jwt.sign(
+            {_id: newUser._id, email: newUser.email, firstName: newUser.firstName, lastName: newUser.lastName, age: newUser.age}, SECRET
+        );
+        console.log('JWT:', userToken);
+        res.status(201).cookie('userToken', userToken, {expires: new Date(Date.now() + 90000000000000)}).json({successMessage: 'user created', user: newUser});
+    } catch (error) {
+        console.log('Register Error:', error);
+        res.status(400).json(error);
+    }
+};
