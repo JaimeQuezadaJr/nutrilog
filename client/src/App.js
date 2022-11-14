c7bab46946f181801d2ad8f11373143f3645b9db
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-13 20:28:20 -0800

    edited styling on app js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <BrowserRouter>
         <Routes>
           <Route path={'/'} element={<Home/>} />
