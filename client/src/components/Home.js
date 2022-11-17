aadd4d24fd4c04c2ef4dbf144cc6882caf2c7620
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-17 11:04:26 -0800

    added nutrient handler function and states

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,9 +5,12 @@ import axios from 'axios'
 const Home = () => {
     const [food, setFood] = useState([])
     const[foodQuery, setFoodQuery] = useState("")
-    const onSubmitHandler = (e) => {
+    const [foodIndex, setFoodIndex] = useState(null)
+    const [foodNutrients, setFoodNutrients] = useState([])
+    const foodHandler = (e) => {
         e.preventDefault();
         console.log(foodQuery)
+        console.log(foodIndex)
         const params = {
             api_key: 'ma4EHu5hkEyGjQ5cwZeB9BjYd9iMg6XxzzmXqkMV',
             query: `${foodQuery}`,
@@ -15,26 +18,46 @@ const Home = () => {
             pagesize: 10,
         }
         console.log('hello')
-        
-        
+    
             axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(params.api_key)}&query=${encodeURIComponent(params.query)}&dataType=${encodeURIComponent(params.dataType)}&pageSize=${encodeURIComponent(params.pagesize)}`)
             .then(res => {
-                console.log(res.data.foods)
-                console.log(res.data.foods[0].foodNutrients)
                 setFood(res.data.foods)
                 setFoodQuery("")
+                console.log(res.data)
+                console.log(res.data.foods)
+                console.log(res.data.foods[foodIndex].foodNutrients)
             })
             .catch((err) => {
                 console.log('Something went wrong', err)
             })
-        
-        
-
+    }
+    const nutrientHandler = (e) => {
+        console.log(foodQuery)
+        console.log(foodIndex)
+        const params = {
+            api_key: 'ma4EHu5hkEyGjQ5cwZeB9BjYd9iMg6XxzzmXqkMV',
+            query: `${foodQuery}`,
+            dataType: ['Survey (FNDDS)'],
+            pagesize: 10,
+        }
+        console.log('hello')
+    
+            axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(params.api_key)}&query=${encodeURIComponent(params.query)}&dataType=${encodeURIComponent(params.dataType)}&pageSize=${encodeURIComponent(params.pagesize)}`)
+            .then(res => {
+                setFood(res.data.foods)
+                setFoodQuery("")
+                console.log(res.data)
+                console.log(res.data.foods)
+                console.log(res.data.foods[foodIndex].foodNutrients)
+            })
+            .catch((err) => {
+                console.log('Something went wrong', err)
+            })
     }
   return (
     <div>
         <h1>Home</h1>
-        <form onSubmit = {onSubmitHandler}>
+        <form onSubmit = {foodHandler}>
             <p>
                 <label htmlFor='foodQuery'>Food Search</label>
                 <input type='text' value={foodQuery} onChange = {(e) => setFoodQuery(e.target.value)}/>
@@ -42,8 +65,8 @@ const Home = () => {
             <input type="submit" value="Search"/>
         </form>
         {food.map((foods, index)=>
-        <div key={index}>
-            <p>{foods.description}</p>
+        <div key={foods.fdcId}>
+            <button onClick = {nutrientHandler}>{foods.description}{index}</button>
         </div>
         )}
     </div>
