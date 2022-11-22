ec749f584fad0ff7527cacb655ea99e0fdfdccf6
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-21 21:03:13 -0800

    added header componenet

---
+++ client/src/components/Header.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+import Button from 'react-bootstrap/Button'
+
+import Nav from 'react-bootstrap/Nav';
+import styles from './Header.module.css';
