e6e0f0535dd2f08f134b5c26e73594814f35f62a
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-23 19:36:21 -0800

    added loggedin state and props

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Header from './components/Header';
+import { useState } from 'react';
 
 function App() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className='App'>
       <BrowserRouter>
-      <Header/>
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
         <Routes>
           <Route path={'/'} element={<Home/>} />
         </Routes>
