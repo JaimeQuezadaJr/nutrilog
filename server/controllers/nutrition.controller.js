9c66cb1ee88c240b263d5b48b4ac6a38d3b619ac
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-26 20:18:31 -0800

    added nutrition find all method

--- server/controllers/nutrition.controller.js
+++ server/controllers/nutrition.controller.js
@@ -2,3 +2,16 @@ const Nutrition = require('../models/nutrition.model');
 const jwt = require('jsonwebtoken');
 const SECRET = process.env.JWT_SECRET;
 const User = require('../models/user.model');
+module.exports = {
+    findAllNutritions: (req, res) => {
+    Nutrition.find({})
+      .populate('createdBy', 'firstName lastName age email')
+      .then((nutritions) => {
+        res.json(nutritions);
+      })
+      .catch((err) => {
+        console.log('ERROR IN Get all', err);
+        res.status(400).json({ message: 'something went wrong in find all nutrition goals', error: err });
+      });
+  },
+};
