e8e701c8ce7a73993a4779f4ed6336170baebba5
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-30 18:23:09 -0800

    added nutrition authenticated routes

--- server/routes/nutrition.routes.js
+++ server/routes/nutrition.routes.js
@@ -1,2 +1,11 @@
 const NutritionController = require('../controllers/nutrition.controller');
 const { authenticate } = require('../config/jwt.config'); //TODO include if implement JWT
+
+module.exports = (app) => { 
+    app.get('/api/nutrition', NutritionController.findAllNutritions);
+    app.get('/api/nutrition/user/:id', authenticate, NutritionController.findNutritionByUser); 
+    app.get('/api/nutrition/:id', authenticate, NutritionController.findOneNutrition);
+    app.post('/api/nutrition', authenticate, NutritionController.createNutrition);
+    app.put('/api/nutrition/:id', authenticate, NutritionController.updateNutrition);
+    app.delete('/api/nutrition/:id', authenticate, NutritionController.deleteNutrition); 
+}
