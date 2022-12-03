85cd5379ab6bb28c36e655387c87d4608b22e8e5
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-12-02 17:57:53 -0800

    added about component

---
+++ client/src/components/About.js
@@ -0,0 +1,7 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
