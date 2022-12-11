9c9a186a81c9c84659cfad4d0e93888a02a39c10
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-12-10 22:26:09 -0800

    completed login component

--- client/src/components/UserLogin.js
+++ client/src/components/UserLogin.js
@@ -6,3 +6,56 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import '../UserLogin/UserLogin.module.css'
 import Card from 'react-bootstrap/Card';
+
+const UserLogin = ({ setLoggedIn }) => {
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState({})
+    const [errors, setErrors] = useState({})
+    const [user, setUser] = useState({
+      email: '',
+      password: '',
+    });
+    const handleChange = (e) => {
+      setUser({
+        ...user,
+        [e.target.name]: e.target.value,
+      });
+    };
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      axios
+        .post('http://localhost:8000/login', user, { withCredentials: true })
+        .then((res) => {
+          console.log(res.data);
+          setLoggedIn(true);
+          navigate('/dashboard');
+        })
+        .catch((err) => setErrors(err.response));
+    };
+    return (
+      <>
+        <Form onSubmit={handleSubmit} className="form"> 
+          <Container>
+            <Form.Group className="mb-3 col-md-4" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" name="email" placeholder="Enter email" value={user.email} onChange={handleChange} required />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+  
+            <Form.Group className="mb-3 col-md-4" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" name="password" placeholder="Password" value={user.password} onChange={handleChange} required/>
+              {errors.data && <Form.Text className='text-danger'>{errors.data.error}</Form.Text>}
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Container>
+        </Form>
+        </>
+    );
+  };
+  
+  export default UserLogin;
