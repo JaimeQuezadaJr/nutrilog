5b446695d61c64be286cb3e2e871e60585a6a34f
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-12-12 20:05:08 -0800

    added new goal model properties

--- server/models/goal.model.js
+++ server/models/goal.model.js
@@ -1,1 +1,25 @@
 const mongoose = require('mongoose');
+
+const GoalSchema = mongoose.Schema (
+    {
+        maxDailyCalories: {
+            type: Number,
+            required: [true, 'Please enter desired max daily calories']
+        },
+        foodTime: {
+            type: String,
+            required: [true, 'Please enter breakfast, lunch or dinner']
+        },
+        foodType: {
+            type: String,
+            required: [true, 'Please enter food type']
+        },
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Goal = mongoose.model('goal', GoalSchema);
+module.exports = Goal;
