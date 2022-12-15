75906ad0f1aa944eb3c6d20a136295d141961137
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-12-14 17:13:56 -0800

    added user registration component

---
+++ client/src/components/UserRegistration.js
@@ -0,0 +1,6 @@
+import { useState } from 'react'
+import axios from 'axios' 
+import { useNavigate } from 'react-router-dom'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
