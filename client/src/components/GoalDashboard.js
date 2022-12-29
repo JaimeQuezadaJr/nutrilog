4337afad4d816b6c326fdf5e0c7cbb6686600e03
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-12-28 19:48:45 -0800

    added goal dashboard component

---
+++ client/src/components/GoalDashboard.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import GoalList from "../GoalList/GoalList";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
