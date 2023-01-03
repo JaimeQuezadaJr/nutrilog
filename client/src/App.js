0ebc4040f797c930720a85cfc44ed0a98519a9aa
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-01-02 20:05:48 -0800

    added goal add component route

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ function App() {
           <Route path={'/login'} element={<UserLogin setLoggedIn={setLoggedIn} />} />
           <Route path={'/register'} element={<UserRegistration setLoggedIn={setLoggedIn} />}/>
           <Route path={'/dashboard/'} element={<GoalDashboard setLoggedIn={setLoggedIn} />} />
+          <Route path={'/goal/add/:category'} element={<GoalAdd setLoggedIn={setLoggedIn} />} />
           <Route path={'/about'} element={<About setLoggedIn={setLoggedIn} />} />
         </Routes>
       </BrowserRouter>
