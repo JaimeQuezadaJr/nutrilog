f1ef7ed25ea18962eb9560324787107c36b3b196
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-01-31 15:21:15 -0800

    delete food component updated to include date added

--- client/src/components/DeleteFood.js
+++ client/src/components/DeleteFood.js
@@ -2,12 +2,16 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import Button from 'react-bootstrap/esm/Button';
+import Container from 'react-bootstrap/esm/Container';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const DeleteFood = ({setLoggedIn}) => {
     const [user, setUser] = useState("");
     const [food, setFood] = useState([]);
-    const [nutrition, setNutrition] = useState([]);
-    const { id } = useParams();
+
+
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -21,6 +25,7 @@ const DeleteFood = ({setLoggedIn}) => {
             //TODO get all three goals from backend /api/nutrition/user/:id
             axios.get(`http://localhost:8000/api/nutrition/user/${res.data._id}`, { withCredentials: true})
             .then(res => {
+                console.log(res.data)
               setFood(res.data)
             })
             .catch(err => console.log(err));
@@ -40,15 +45,34 @@ const DeleteFood = ({setLoggedIn}) => {
           })
           .catch((err) => console.log(err));
       };
+      const dateParse = (date) => {
+        let dateObj = new Date(date);
+        let day = `0${dateObj.getDate()}`.slice(-2);
+        let month = `0${dateObj.getMonth() + 1}`.slice(-2);
+        return `${month}/${day}/${dateObj.getFullYear()}`;
+      }
   return (
-    <div className='form'>
+    <Container>
+        <Row className='form'>
+            <Col>
+            <Card border="light" >
+                        <Card.Header>Foods</Card.Header>
+                        <Card.Body>
+        
+    <div>
                     {food.map((foods, index)=>
-                            <div key={index}>
-                                <p className='m-0'>{foods.foodTitle}</p>
-                                <Button variant="outline-danger" className='' size='sm' onClick = {(e) => {deleteMovie(foods._id)}}>Delete</Button>
+                            <div key={index} className='delete-food'>
+                                <p className='m-0'>{foods.foodTitle}<span className='small-date ms-3'>{dateParse(foods.createdAt)}</span></p>
+                                
+                                <Button variant="outline-danger" className='m-1' size='sm' onClick = {(e) => {deleteMovie(foods._id)}}>Delete</Button>
                             </div>
                     )}
                     </div>
+                    </Card.Body>
+                    </Card>
+                    </Col>
+                    </Row>
+    </Container>
   )
 }
 
