d52cc14887a98d02a854da5d3ac11642235a7c15
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-02-27 17:21:51 -0800

    cleaned imports

--- client/src/components/GoalList.js
+++ client/src/components/GoalList.js
@@ -1,12 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
 import Button from "react-bootstrap/Button";
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import styles from "./GoalList.module.css";
 import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 
 const GoalList = ({setGoals, goals, category, complete, setComplete}) => {
