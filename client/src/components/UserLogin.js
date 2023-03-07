cb0a3d878a6c41403572c426e6d89ac7b740ed51
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-03-06 20:57:09 -0800

    removed imports row and col

--- client/src/components/UserLogin.js
+++ client/src/components/UserLogin.js
@@ -5,9 +5,6 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-import Card from 'react-bootstrap/Card';
 import { motion } from 'framer-motion';
 
 const UserLogin = ({ setLoggedIn }) => {
